2bb6ab9b4761d148f341163d27bc83f2a5931ad0
abhishekjainmangosciences <abhishek.jain@mangosciences.com>
2021-12-09 23:48:50 +0530

    learning babel

---
+++ input.js
@@ -0,0 +1,4 @@
+const hello = () => console.log("hello world!")
+
+let text = "Hello world, welcome to the universe.";
+text.includes("world") 
---
+++ output.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var hello = function hello() {
+  return console.log("hello world!");
+};
+
+var text = "Hello world, welcome to the universe.";
+text.includes("world");
