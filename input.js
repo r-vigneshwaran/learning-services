b5fec54cadd3734c40f1671af5f1e20ebf58c59e
vignesh <vignesh@tdgpartner.info>
2022-01-13 23:57:01 +0530

    removed old webpack files

--- input.js
+++
@@ -1,4 +0,0 @@
-const hello = () => console.log("hello world!")
-
-let text = "Hello world, welcome to the universe.";
-text.includes("world") 
--- output.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var hello = function hello() {
-  return console.log("hello world!");
-};
-
-var text = "Hello world, welcome to the universe.";
-text.includes("world");
