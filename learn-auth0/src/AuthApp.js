6212ee739f53f4a9361721c7d2ad5476400b1ac7
vignesh <vignesh@tdgpartner.info>
2022-02-20 21:36:49 +0530

    added routes

--- learn-auth0/src/AuthApp.js
+++ learn-auth0/src/AuthApp.js
@@ -1,38 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import axios from 'axios';
+import { Route, Routes } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home';
+import Landing from './pages/Landing';
 
 const AuthApp = () => {
-  const {
-    loginWithPopup,
-    loginWithRedirect,
-    logout,
-    user,
-    isAuthenticated,
-    isLoading,
-    getAccessTokenSilently
-  } = useAuth0();
-  const [message, setMessage] = useState('none');
+  const { isLoading } = useAuth0();
 
-  const get = async () => {
-    const res = await axios.get('http://localhost:1234/');
-    setMessage(res.data);
-  };
-
-  const getProtected = async () => {
-    try {
-      const token = await getAccessTokenSilently();
-      const headers = {
-        headers: {
-          authorization: `Bearer ${token}`
-        }
-      };
-      const res = await axios.get('http://localhost:1234/protected', headers);
-      setMessage(res.data);
-    } catch (err) {
-      setMessage(`Reason ${err.message}`);
-    }
-  };
   if (isLoading)
     return (
       <div>
@@ -40,35 +15,10 @@ const AuthApp = () => {
       </div>
     );
   return (
-    <div className="App-body">
-      <h1>Auth0 Authentication</h1>
-      <h2>
-        {isAuthenticated &&
-          `Hi ${user?.given_name ? user?.given_name : user?.nickname}`}
-      </h2>
-      <ul>
-        <li>
-          <button onClick={loginWithPopup}>Login with Popup</button>
-        </li>
-        <li>
-          <button onClick={loginWithRedirect}>Login with Redirect</button>
-        </li>
-        <li>
-          <button onClick={logout}>Logout</button>
-        </li>
-        <li>
-          <button onClick={get}>Get unprotected</button>
-        </li>
-        <li>
-          <button onClick={getProtected}>Get protected</button>
-        </li>
-      </ul>
-      <div className="container">
-        <h3>User is {isAuthenticated ? 'Logged in' : 'Logged Out'}</h3>
-        {isAuthenticated && <pre>{JSON.stringify(user, null, 2)} </pre>}
-        <h5>{message}</h5>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/home" element={<PrivateRoute component={Home} />} />
+    </Routes>
   );
 };
 
